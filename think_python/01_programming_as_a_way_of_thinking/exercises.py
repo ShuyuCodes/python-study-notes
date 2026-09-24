59c4a5516a052ddc551dbeedd5bf1e01878a8989
ShuyuCodes <ms.zhangshuyu@gmail.com>
2026-09-24 13:48:25 -0400

    fixed some lost notes from chapter 01's book examples

---
+++ think_python/01_programming_as_a_way_of_thinking/exercises.py
@@ -0,0 +1,8 @@
+#1.9.1 exercises
+
+print(round(42.5))
+
+print(round(43.5))
+
+print(round(0.5))
+
--- think_python/09_lists/book_examples.py
+++ think_python/09_lists/book_examples.py
@@ -1,7 +1,10 @@
+
+
+
 #9.8.Sorting lists
 
 text ="when+I+push+to+github+it+asked+for+my+user+name+for+github.com+and+also+password+for+whatever+I+inputed+what+should+I+fill+in"
 l = text.split("+")
 print(l)
 
-print(" ".join(l), "\n\nIn the end when I entered my username of my github account and password for it, it still failed.")
+print(" ".join(l), "\n\nIn the end when I entered my username of my github account and my generated PERSONAL ACCESS TOKEN as a password for it, it SUCCEEDED!!!")
